2ec5d80691842865cd858d875e80088d4fda6d3d
hailong <longlongh4@gmail.com>
2013-04-27 16:21:15 +0800

    change default size

--- CuteAlbum/main.cpp
+++ CuteAlbum/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     QtQuick2ApplicationViewer viewer;
     viewer.setMainQmlFile(QStringLiteral("qml/CuteAlbum/main.qml"));
-    viewer.showExpanded();
+    viewer.showMaximized ();
 
     return app.exec();
 }
